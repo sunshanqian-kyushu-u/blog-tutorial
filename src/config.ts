98772204061b684c48355b2bf54fef682d9709a5
sunshanqian <sunshanqian@outlook.com>
2025-05-24 21:17:43 +0900

    navbar03

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,32 @@ type SiteConfig = {
 export const siteConfig: SiteConfig = {
     subtitle: "Tutorial", 
 }
+
+type Link = {
+    name: string
+    url: string
+    external: boolean
+}
+
+export const NavbarLinks: Link[] = [
+    {
+        name: "Home", 
+        url: "/", 
+        external: false
+    }, 
+    {
+        name: "Archive", 
+        url: "/archive", 
+        external: false
+    }, 
+    {
+        name: "About", 
+        url: "/about", 
+        external: false
+    }, 
+    {
+        name: "GitHub", 
+        url: "https://github.com", 
+        external: true
+    }
+]
