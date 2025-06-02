dddff4a87113681cc5018fcaeff5390e68f742d0
sunshanqian <sunshanqian@outlook.com>
2025-06-02 22:36:28 +0900

    Navbar 07

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon()],
+  adapter: netlify(),
 });
