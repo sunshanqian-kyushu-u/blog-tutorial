90116c006fa4c45817e551f24786c35b4b6cd560
sunshanqian <sunshanqian@outlook.com>
2025-06-19 23:38:00 +0900

    Tag

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,11 +5,11 @@ import { defineCollection, z } from 'astro:content';
 // 注：好像在新版的内容集合中不需要导入 glob 和 file 这种加载器
 const blog = defineCollection({ 
     schema: z.object({
-        // id: z.number(),
         title: z.string(),
         // 要注意 2022-07-01 的不是字符串而是日期
         published: z.date(),
         category: z.string(),
+        tags: z.array(z.string()),
         draft: z.boolean().optional(),
     }),
  });
