56e1cc09c9ff2051403f36468738f926f4c6ab3c
sunshanqian <sunshanqian@outlook.com>
2025-06-23 20:48:19 +0900

    Markdown

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	// darkMode: 'media',
@@ -9,5 +11,5 @@ export default {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [typography],
 }
